3be77353443cd3eeeeb123c658b798133d2b5b35
ribeiro3k <ribeiro3k@gmail.com>
2025-06-21 12:25:54 -0400

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -11,10 +11,8 @@ let supabaseInstance: SupabaseClient | null = null;
 if (supabaseUrl && supabaseAnonKey) {
   supabaseInstance = createClient(supabaseUrl, supabaseAnonKey);
 } else {
+  // This console error will use the generic SUPABASE_ERROR_MESSAGE from constants.ts
   console.error(SUPABASE_ERROR_MESSAGE); 
-  // In a real app, you might want to throw an error or display a global message
-  // For this exercise, we'll allow the app to load but features relying on Supabase will fail.
-  // Components should check if supabase is null before using it.
 }
 
 export const supabase = supabaseInstance;
